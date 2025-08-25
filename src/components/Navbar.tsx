010f1e678e908f418bc436f6fd49f782960af84e
javi2754 <jjaaiiggaanneesshh77@gmail.com>
2025-08-25 15:18:14 +0530

    updated navbar for responsive view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,55 +9,35 @@ import {
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-900 m-2 px-6 py-3 shadow-md row ">
-      <h1 className="text-2xl font-bold">DevTracker</h1>
-
-      <div className="col-1 col-lg-2  flex justify-between items-center border rounded">
-        {/* Links */}
-        <ul className="flex list-unstyled flex-wrap justify-between sm:space-x-6">
-          <li className="px-2 py-3">
-            <Link
-              to="/"
-              className="text-color text-decoration-none hover:text-gray-300"
-            >
-              <Home className="me-2 text-color" size={24} />
-              <span className="d-none d-lg-inline">Home</span>
+    <nav className="d-flex flex-column" style={{ height: "100vh" }} >
+      <div>
+        <h1 className="text-dark mx-3">Dev Tracker</h1>
+      </div>
+      <div className="d-flex align-items-end align-items-md-center justify-content-center justify-content-md-start "style={{ height: "100vh" }} >
+        <ul className="list-unstyled shadow  rounded m-3 d-inline-flex  flex-row flex-md-column gap-5">
+          <li className="p-3 ">
+            <Link to="/" className="text-color text-decoration-none" >
+              <Home className="text-dark" size={24} />
             </Link>
           </li>
-          <li className="px-2 py-3">
-            <Link
-              to="/about"
-              className="text-color text-decoration-none hover:text-gray-300"
-            >
-              <MessageSquareMore className="me-2 text-color" size={24} />
-              <span className="d-none d-lg-inline">Messages</span>
+          <li className="p-3">
+            <Link to="/about" className="text-color text-decoration-none">
+              <MessageSquareMore className="text-dark" size={24} />
             </Link>
           </li>
-          <li className="px-2 py-3">
-            <Link
-              to="/projects"
-              className="text-color text-decoration-none hover:text-gray-300"
-            >
-              <Users className="me-2 text-color" size={24} />
-              <span className="d-none d-lg-inline">Tasks</span>
+          <li className="p-3">
+            <Link to="/projects" className="text-color text-decoration-none">
+              <Users className="text-dark" size={24} />
             </Link>
           </li>
-          <li className="px-2 py-3">
-            <Link
-              to="/contact"
-              className="text-color text-decoration-none hover:text-gray-300"
-            >
-              <ListTodo className="me-2 text-color" size={24} />
-              <span className="d-none d-lg-inline">Members</span>
+          <li className="p-3 ">
+            <Link to="/contact" className="text-color text-decoration-none">
+              <ListTodo className="mx-auto text-dark" size={24} />
             </Link>
           </li>
-          <li className="px-2 py-3">
-            <Link
-              to="/settings"
-              className="text-color text-decoration-none hover:text-gray-300"
-            >
-              <Settings className="me-2 text-color" size={24} />
-              <span className="d-none d-lg-inline">Settings</span>
+          <li className="p-3 ">
+            <Link to="/settings" className="text-color text-decoration-none">
+              <Settings className="text-dark" size={24} />
             </Link>
           </li>
         </ul>
