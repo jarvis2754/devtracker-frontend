a8768786f37d8ed1dff0b5c931c1a836c25b1ce9
javi2754 <jjaaiiggaanneesshh77@gmail.com>
2025-08-25 11:05:48 +0530

    react router dom setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
-  return (<>Hello world</>)
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Home />} />
+        <Route path="/projects" element={<Home />} />
+        <Route path="/contact" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
